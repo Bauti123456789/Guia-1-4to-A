e4b0e54a59d2211b9f5fcb00e8aadb4a5cdc0cd3
Bauti123456789 <bautijantus@gmail.com>
2025-03-25 11:44:44 -0300

    ejercicio 1 finalizado

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int cantidadAlumnos;
+    float magnitudTerremoto;
+    string nombreAlumno;
+    bool compuHabilitada;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cantidadAlumnos = 24;
+        magnitudTerremoto = 9.3f;
+        nombreAlumno = "Bautista Jantus";
+        compuHabilitada = false;
+
+        Debug.Log(cantidadAlumnos);
+        Debug.Log(magnitudTerremoto);
+        Debug.Log(nombreAlumno);
+        Debug.Log(compuHabilitada);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
