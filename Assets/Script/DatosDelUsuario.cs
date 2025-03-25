910f6f35c976a4db8f0a2e8076d9f4f0373fadcb
Bauti123456789 <bautijantus@gmail.com>
2025-03-25 12:15:22 -0300

    ejercicio 1.2.3 finalizado

---
+++ Assets/Script/DatosDelUsuario.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    public string nombreUsuario;
+    [SerializeField] int edadUsuario;
+    public float estaturaUsuario;
+    public bool donanteDeOrganos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario);
+        Debug.Log("Tengo " + edadUsuario + "años");
+        Debug.Log("Mido " + estaturaUsuario + "metros");
+        Debug.Log("Es " + donanteDeOrganos + " que soy donante de organos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
